88913aa7706527cdd9f6f4c977f828f334c3e195
Shakira <shakira.r.salazar@gmail.com>
2021-08-31 15:13:57 +0100

    balance attr

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
-# NameError
-
+# NameError, /spec/oystercard_spec.rb, 1
+require 'oystercard'
 describe Oystercard do
-  
+  it 'Should initialize with a balance of 0' do
+		expect (subject.balance) == 0
+	end
 end
