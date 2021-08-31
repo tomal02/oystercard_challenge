26c3390bb2e883bb2542dbb3af3f6ba41b874077
Shakira <shakira.r.salazar@gmail.com>
2021-08-31 16:33:58 +0100

    added deduct method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,22 @@
 class Oystercard
 	attr_reader :balance
+	MAX_LIMIT = 90
 	def initialize
 		@balance = 0
 	end
 
   def top_up(amount)
+		fail "#{amount} exceeds #{MAX_LIMIT}" if max?(amount)
     @balance += amount
+  end
+
+	def max?(amount)
+		if (@balance + amount) > MAX_LIMIT
+			true
+		end
+	end
+
+  def deduct(fare)
+    @balance -= fare
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,12 +5,26 @@ describe Oystercard do
 		expect (subject.balance) == 0
 	end
 
-	it 'Should top up balance' do
-		subject.top_up(10)
-		expect(subject.balance) == 10
+	describe '#top_up' do
+
+		it 'Should top up balance' do
+			subject.top_up(10)
+			expect(subject.balance) == 10
+		end
+
+		it {is_expected.to respond_to(:top_up).with(1).argument }
+
+		it 'Should raise an error if the balance exceeds max limit' do
+			amount = 100
+			expect{subject.top_up(amount)}.to raise_error "#{amount} exceeds #{Oystercard::MAX_LIMIT}"
+		end
 	end
 
-	describe '#top_up' do
-		it {is_expected.to respond_to(:top_up).with(1).argument }
+	describe '#deduct' do
+		it 'Should deduct from balance' do
+			subject.top_up(10)
+			subject.deduct(5)
+			expect(subject.balance) == 5
+		end
 	end
 end
